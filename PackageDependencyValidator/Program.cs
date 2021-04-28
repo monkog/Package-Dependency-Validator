764f0eaeb24d18ba229ea7d461cc549910319d97
Monika Kogut <monkog@wp.pl>
2021-04-28 14:20:09 +0200

    Setup ioc container

--- PackageDependencyValidator/Program.cs
+++ PackageDependencyValidator/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Autofac;
 
 namespace PackageDependencyValidator
 {
-	class Program
+	public class Program
 	{
+		public static IContainer Container { get; set; }
+
 		static void Main(string[] args)
 		{
+			var builder = new ContainerBuilder();
+			Container = builder.Build();
 		}
 	}
 }
