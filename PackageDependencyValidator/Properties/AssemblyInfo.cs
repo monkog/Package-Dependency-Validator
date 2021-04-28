759b2b7377d3e539cbf44753823978350da33fc5
Monika Kogut <monkog@wp.pl>
2021-04-28 15:30:49 +0200

    Fix renamed package in assembly info

--- PackageDependencyValidator/Properties/AssemblyInfo.cs
+++ PackageDependencyValidator/Properties/AssemblyInfo.cs
@@ -13,4 +13,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("DependencyResolverTests")]
+[assembly: InternalsVisibleTo("PackageDependencyValidatorTests")]
