d3ad13b7e04fe488bd4a88d110c0b7a5bb999b4a
Monika Kogut <monkog@wp.pl>
2021-04-28 14:11:40 +0200

    Empty project setup

---
+++ PackageDependencyValidator/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PackageDependencyValidator")]
+[assembly: AssemblyDescription("Validates the package installation in terms of package dependency resolution.")]
+[assembly: AssemblyProduct("PackageDependencyValidator")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c7125cc0-92ea-4e57-a7a5-9c3839fa7cd1")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("DependencyResolverTests")]
---
+++ PackageDependencyValidatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PackageDependencyValidatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PackageDependencyValidatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("13ff367e-5ed6-4ee5-9646-cde368d6611e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
